1eeb2b59be19bdfc764b2b451e9402220710a192
Joe Ren <earthengine@gmail.com>
2014-09-07 20:32:35 +1000

    Add ajax example

---
+++ interface/new_card.php
@@ -0,0 +1,6 @@
+<div class="box-row-card" id="<?php echo($_REQUEST['id']); ?>">
+    <img src="image/cards/ANZ_Rewards_Black.jpg" alt="ANZ Rewards Black">
+    <div class="div-text">ANZ Rewards Black With AMEX ($4000)</div>
+    <input type="button" value="delete"/>
+</div>
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-function app1(){
-  return "<div class=\"box-row-card\"><img src=\"image/cards/ANZ_Rewards_Black.jpg\" alt=\"ANZ Rewards Black\"><div class=\"div-text\">ANZ Rewards Black With AMEX ($4000)</div><input type=\"button\" value=\"delete\"/></div>";
+function app1(cont){
+    var req = $.ajax({
+	url: "interface/new_card.php",
+	type: "GET",
+	data: { id: 1 },
+	dataType: "html"});
+    req.done(cont);
 }
 
 function deleteCard(event){
@@ -8,9 +13,11 @@ function deleteCard(event){
 
 $(document).ready(function(){
   $("#addCard").click(function(){
-    var v1=$(app1());
-    v1.children("input").click(deleteCard);
-    $("#wallet").append(v1);
+       app1(function(msg){
+       var v1=$(msg);
+       v1.children("input").click(deleteCard);
+       $("#wallet").append(v1);
+    });
   });
   $("input[value=\"delete\"]").click(deleteCard);
 });
