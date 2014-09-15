64b1313d8047fbceed4094582f05820eb797303c
Dong Xuening <neodaking@gmail.com>
2014-09-15 11:53:12 +1000

    Add data into database

---
+++ interface/new_card.php
@@ -0,0 +1,7 @@
+<div class="box-row-card" id="<?php echo($_REQUEST['id']); ?>">
+    <img src="image/cards/ANZ_Rewards_Black.jpg" alt="ANZ Rewards Black">
+    <div class="div-text">ANZ Rewards Black With AMEX ($4000)</div>
+    <input class="delete-card" type="button" value="delete"/>
+    <input class="edit-card" type="button" value="edit"/>
+</div>
+
