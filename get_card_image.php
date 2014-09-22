99361261b8d9211b1b8136fcd96e53cbcebdf114
Joe Ren <s3327627@student.rmit.edu.au>
2014-09-22 21:10:55 +1000

    Update interface

--- get_card_image.php
+++ get_card_image.php
@@ -3,8 +3,8 @@ include 'include/dbconn.php';
 
 $card = $_GET['card'];
 
-$select = "SELECT card_name FROM card_info WHERE id = $card ORDER BY id";
+$select = "SELECT COALESCE(image_url,CONCAT(REPLACE(card_name,' ', '_'),'.jpg')) AS image FROM card_info WHERE id = $card ORDER BY id";
 $rs = mysqli_query($conn, $select);
 $row = mysqli_fetch_array($rs);
-echo $row['card_name'];
+echo $row['image'];
 ?>
