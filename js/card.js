a41b99d5754ada858c0d0608842ff2c9ee6bbb98
Joe Ren <s3327627@student.rmit.edu.au>
2014-10-10 11:29:34 +1100

    updated card.js

--- js/card.js
+++ js/card.js
@@ -1,5 +1,6 @@
 function Wallet(){
 	var cards = [];
+	var uicards = [];
 	
 	var findIdx(id){
 		for(var i=0; i<cards.length; ++i){
@@ -10,26 +11,32 @@ function Wallet(){
 	}
 	
 	return {
+		getUiCard : function(id){
+			return uicards[findIdx(id)];
+		}
 		getCard : function(id){
 			return cards[findIdx(id)];
 		}
-		addCard : function(id,monthly_spend,balance_percent,amex_percent){
-			cards.push({"id":id,
-				"monthly_spend":monthly_spend,
-				"balance_percent":balance_percent,
-				"amex_percent":amex_percent});				
+		addCard : function(uicard){
+			cards.push({"id":uicard.id,
+				"monthly_spend":uicard.ams,
+				"balance_percent":uicard.bpem,
+				"amex_percent":uicard.psa});
+			uicards.push(uicard);
 		}
-		updateCard : function(id,monthly_spend,balance_percent,amex_percent){
-			var idx = findIdx(id);
-			cards[idx].monthly_spend = monthly_spend;
-			cards[idx].balance_percent = balance_percent;
-			cards[idx].amex_percent = amex_percent;
+		updateCard : function(uicard){
+			var idx = findIdx(uicard.id);
+			cards[idx].monthly_spend = uicard.ams;
+			cards[idx].balance_percent = uicard.bpem;
+			cards[idx].amex_percent = uicard.psa;
+			uicards[idx] = uicard;
 		}
 		deleteCard : function(id){
 			cards.splice(idx,1);
+			uicards.splice(ids,1);
 		}
 		toJson : function(){
-			return JSON.stringify();
+			return JSON.stringify(cards);
 		}
 	}
 }
